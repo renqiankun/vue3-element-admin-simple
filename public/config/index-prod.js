f16f94f85d2d454d48614093bc843161ec42f5d5
qiankun <526472759@qq.com>
2022-09-22 18:39:24 +0800

    移除echarts

--- public/config/index-prod.js
+++ public/config/index-prod.js
@@ -1,4 +1,4 @@
 ; (function () {
     window.SITE = {};
-    SITE['baseUrl'] = 'http://www.hbdev.com';
+    SITE['baseUrl'] = '';
 })();
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import element from './element-plus'
 
 
 import svgIcon from './components/SvgIcon.vue' // 全局svg图标组件
-import Container from './components/Container.vue' // 全局svg图标组件
 import Pagination from './components/Pagination.vue' // 
 import datePicker from './components/datePicker.vue' // 日期范围组件
 import Table from './components/Table.vue' // 
@@ -20,7 +19,6 @@ app.use(router)
 app.use(store)
 app.use(element)
 app.component('svg-icon', svgIcon)
-app.component('Container', Container)
 app.component('Pagination', Pagination)
 app.component('datePicker', datePicker)
 app.component('Table', Table)
